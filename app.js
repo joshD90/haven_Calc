1cea5587c347471ac0a2455fdd748498ca19168d
Joshua Dancey <joshuadancey@hotmail.com>
2022-01-04 00:56:43 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.post('/result', (req, res) => {
+  const firstNum = parseFloat(req.body.number1);
+  const secondNum = parseFloat(req.body.number2);
+  const operate = req.body.operator;
+
+  myResult = JSON.stringify(doCalc(firstNum, secondNum, operate));
+
+
+  res.write(myResult);
+  res.end();
+});
+
+app.listen(3000, () => {
+  console.log('server is listening on Port 3000');
+});
+
+function doCalc(num1, num2, bidniz) {
+  var result = 0;
+  switch (bidniz) {
+    case "add":
+      result = num1 + num2;
+      break;
+    case "subtract":
+      result = num1 - num2;
+      break;
+    case "multiply":
+      result = num1 * num2;
+      break;
+    case "divide":
+      result = num1 / num2;
+      break;
+    case "powerOf":
+      result = num1 ** num2;
+      break;
+    case "rootOf":
+      result = num1 ** -num2;
+      break;
+  }
+  return result;
+}
---
+++ public/script.js
@@ -0,0 +1,1 @@
+alert('java working hyup');
